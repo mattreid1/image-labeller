06a81438da1243132bf2ffa2a4e3700d2ea8a13b
Matt Reid <matthew.reid8@nhs.net>
2020-10-21 14:13:41 +0100

    Added client and server code.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 const express = require("express");
-const app = express();
+const fs = require("fs-extra");
 
+const app = express();
 const PORT = 3000;
+const outputDir = "./static/output/";
+fs.ensureDirSync(outputDir);
+
+app.get("/api/listImages", (req, res) => {
+	const images = fs.readdirSync("./static/images/");
+	res.setHeader('Content-Type', 'application/json');
+	res.json(images);
+});
+
+app.get("/api/label", (req, res) => {
+	const label = req.query.label;
+	const image = req.query.image;
+
+	fs.ensureDirSync(outputDir + label);
+	fs.moveSync(`./static/images/${image}`, `${outputDir}${label}/${image}`);
+
+	res.setHeader('Content-Type', 'application/json');
+	res.json({ "success": true });
+});
+
+app.get("/api/undo", (req, res) => {
+	const label = req.query.label;
+	const image = req.query.image;
+
+	fs.moveSync(`${outputDir}${label}/${image}`, `./static/images/${image}`);
+
+	res.setHeader('Content-Type', 'application/json');
+	res.json({ "success": true });
+});
 
 app.use(express.static("static"));
 app.listen(PORT);
---
+++ src/static/app.js
@@ -0,0 +1,67 @@
+var i = 0;
+var labelStack = [];
+const labels = ["Keep", "Reject"];
+
+$(document).ready(function () {
+	for (let i = 0; i < labels.length; i++) {
+		const label = labels[i];
+		$("#labelRow").append(`<button type="button" class="btn btn-primary" id="labelBtn${i}" style="margin-right:12px;">${label} (${i + 1})</button>`);
+	}
+
+	$.getJSON("/api/listImages", (imageArr) => {
+		changeImage(imageArr[i]);
+		$("#progress").html(`${i}/${imageArr.length} (${parseFloat((i/imageArr.length * 100).toFixed(2))}% done)`)
+
+		$("body").keypress((eventData) => {
+			const key = eventData.key.toLowerCase();
+			if (isNumeric(key)) {
+				if (parseInt(key) <= labels.length) handleLabel(imageArr[i], parseInt(key));
+			}
+
+			if (key == "q") {
+				undo();
+			}
+		});
+
+		function handleLabel(image, label) {
+			$("#undoBtn").prop('disabled', false);
+			i++;
+			labelStack.push({ "image": image, "label": label });
+			$.getJSON(`/api/label?image=${image}&label=${label}`); // Assume worked?
+			changeImage(imageArr[i]);
+			$("#progress").html(`${i}/${imageArr.length} (${parseFloat((i/imageArr.length * 100).toFixed(2))}% done)`);
+			alertMsg("primary", `Marked '${image}' as '${labels[label-1]}'`);
+		}
+
+		function undo() {
+			if (i > 0) {
+				const item = labelStack.pop();
+				$.getJSON(`/api/undo?image=${item.image}&label=${item.label}`, () => {
+					i--;
+					changeImage(imageArr[i]);
+					alertMsg("warning", `Undid '${item.image}'`);
+				});
+				if (i == 0) {
+					$("#undoBtn").prop('disabled', true);
+				}
+			} else {
+				alertMsg("danger", "Cannot undo any further!");
+			}
+		}
+
+		function changeImage(image) {
+			$("#image").attr("src", `/images/${image}`);
+		}
+	});
+});
+
+function alertMsg(type, content) {
+	$("#alerts").html(`<div class="alert alert-${type} alert-dismissible fade show" role="alert">${content}<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>`);
+}
+
+// https://stackoverflow.com/a/175787/3853668
+function isNumeric(str) {
+	if (typeof str != "string") return false // we only process strings!  
+	return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+		!isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
